eef05e0bec2f5503a9c804f4bbc82a29de58edc6
Haci Murat Yaman <haci@muratyaman.co.uk>
2020-04-12 22:27:25 +0100

    benchmark of json responses

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/valyala/fasthttp"
 )
+
+type DemoResponse struct {
+	Data string `json:"data"`
+	Ts   string `json:"ts"`
+}
 
 var (
 	addr = flag.String("addr", ":8080", "TCP address to listen to")
@@ -15,7 +23,7 @@ var (
 func main() {
 	flag.Parse()
 
-	h := requestHandler
+	h := requestHandlerJson
 
 	if err := fasthttp.ListenAndServe(*addr, h); err != nil {
 		log.Fatalf("Error in ListenAndServe: %s", err)
@@ -25,4 +33,19 @@ func main() {
 func requestHandler(ctx *fasthttp.RequestCtx) {
 	fmt.Fprintf(ctx, "Hello, world!\n\n")
 	ctx.SetContentType("text/plain; charset=utf8")
+}
+
+func requestHandlerJson(ctx *fasthttp.RequestCtx) {
+	output := DemoResponse{Data: "hello world", Ts: time.Now().String()}
+	bodyBuffer := new(bytes.Buffer)
+	err := json.NewEncoder(bodyBuffer).Encode(output)
+	if err != nil {
+		ctx.SetStatusCode(500)
+		return
+	}
+
+	ctx.SetContentType("application/json; charset=utf-8")
+	ctx.SetStatusCode(200)
+	ctx.Response.SetBodyString(bodyBuffer.String())
+	//ctx.Response.SetBodyString("{\"data\":\"hello\"}")
 }
